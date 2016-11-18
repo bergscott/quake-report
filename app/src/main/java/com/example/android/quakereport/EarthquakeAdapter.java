550a327e2805f7c25fa9130f76266da5a778baa7
Scott Berg <berg.scott@ymail.com>
2016-11-18 11:01:55 -0600

    split location into separate location offset and primary location fields

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -26,6 +26,9 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     /* date format for displaying the time of day of an earthquake */
     private final DateFormat TIME_FORMAT = new SimpleDateFormat("h:mma", Locale.US);
 
+    private final String LOCATION_OFFSET_INDICATOR = " of ";
+    private final String NO_OFFSET_REPLACEMENT = "Near the";
+
     public EarthquakeAdapter(Context context, ArrayList<Earthquake> earthquakes) {
         super(context, 0, earthquakes);
     }
@@ -50,10 +53,37 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.earthquake_magnitude);
         magnitudeTextView.setText(String.valueOf(currentEarthquake.getMagnitude()));
 
-        // find the text view in the layout for the location and set its text to the current
-        // Earthquake's location
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.earthquake_location);
-        locationTextView.setText(currentEarthquake.getLocation());
+        // get the location String from the current Earthquake and split it into the location offset
+        // and primary location Strings
+        String fullLocation = currentEarthquake.getLocation();
+        String locationOffset;
+        String primaryLocation;
+
+        // if the full location contains a location offset split it and extract the offset and
+        // primary locations
+        if (fullLocation.contains(LOCATION_OFFSET_INDICATOR)) {
+            String[] splitLocation = fullLocation.split(LOCATION_OFFSET_INDICATOR);
+            locationOffset = getContext().getResources().getString(R.string.location_offset,
+                    splitLocation[0]);
+            primaryLocation = splitLocation[1];
+        } else {
+            // the full location does not contain an offset, so replace it with "Near the" and use
+            // the full location for the primary location
+            locationOffset = NO_OFFSET_REPLACEMENT;
+            primaryLocation = fullLocation;
+        }
+
+        // find the text view in the layout for the location offset and set its text to the current
+        // Earthquake's location offset
+        TextView locationOffsetTextView = (TextView) listItemView.findViewById(
+                R.id.earthquake_location_offset);
+        locationOffsetTextView.setText(locationOffset);
+
+        // find the text view in the layout for the primary location and set its text to the current
+        // Earthquake's primary location
+        TextView primaryLocationTextView = (TextView) listItemView.findViewById(
+                R.id.earthquake_primary_location);
+        primaryLocationTextView.setText(primaryLocation);
 
         // get the time from the current Earthquake and convert it into a new Date object
         Date earthquakeDate = new Date(currentEarthquake.getTimeInMilliseconds());
